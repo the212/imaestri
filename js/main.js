9ca565a57310d802e7ccb83abbb17fb644b242d6
Sebastian Soler <the212co@gmail.com>
2015-04-29 20:51:32 -0400

    added css only side menu and responsive menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 $(function() {
-  // Handler for .ready() called.
+
+	$(".a-dashboard").click(function() {
+		$('.search').removeClass('search-open');
+		$('.subnav').toggleClass('subnav-open');
+	});
+
+	$(".a-search").click(function() {
+		$('.subnav').removeClass('subnav-open');
+		$('.search').toggleClass('search-open');
+	});
+
+	$("nav ul li a").click(function() {
+		$(this).parent().siblings().children('a').removeClass('active');
+		$(this).toggleClass('active');
+	});
+
 });
