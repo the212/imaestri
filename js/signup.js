0bb8a2a054e0a90433750d1784c3c714f494d537
Sebastian Soler <the212co@gmail.com>
2015-05-05 19:52:36 -0400

    css edits

--- js/signup.js
+++ js/signup.js
@@ -5,12 +5,14 @@ $(function() {
 	$(window).scroll(function() {
 	   if($(window).scrollTop() + $(window).height() == $(document).height()) {
 
+	   	$('.signup-bottom').addClass('bottom-show');
+
 	   		var st = $(this).scrollTop();
 			   if (st > lastScrollTop){
 			       // downscroll code
 			   } else {
 			      // upscroll code
-			      $('.signup-bottom').addClass('bottom-show');
+			      //$('.signup-bottom').addClass('bottom-show');
 			   }
 			   lastScrollTop = st;
 	   }
