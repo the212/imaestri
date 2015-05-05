9d87ec6096010f30b13c58f8df13a9000f285bad
Sebastian Soler <the212co@gmail.com>
2015-05-05 13:26:22 -0400

    finished v1 of mega menu mobile

--- js/main.js
+++ js/main.js
@@ -1,5 +1,30 @@
 $(function() {
 
+	/******************************************
+	********* Primary Nav ******************/
+	$('.nav-trigger').click(function() {
+		$('.primarynav').toggleClass('primarynav-open');
+	});
+
+	$('.primarynav li a').click(function() {
+		$(this).siblings('.primarynav-l2').addClass('primarynav-l2-open');
+	});
+
+	$('.primarynav-l2 li a').click(function() {
+		$(this).siblings('.primarynav-l3').addClass('primarynav-l3-open');
+	});
+
+	$('.l2-back').click(function() {
+		$('.primarynav-l2').removeClass('primarynav-l2-open');
+	});
+
+	$('.l3-back').click(function() {
+		$('.primarynav-l3').removeClass('primarynav-l3-open');
+	});
+
+
+	/******************************************
+	********* My Account Nav ******************/
 	$(".a-dashboard").click(function() {
 		$('.search').removeClass('search-open');
 		$('.subnav').toggleClass('subnav-open');
